39b462409e9cfdffde5257a721e33e242b16c892
tony <attila.kovacs92@gmail.com>
2023-05-02 13:26:05 +0200

    test6

--- resources/js/Components/UI/Contacts.jsx
+++ resources/js/Components/UI/Contacts.jsx
@@ -3,16 +3,16 @@ import { AtSymbolIcon, PhoneIcon, HomeIcon } from "@heroicons/react/20/solid";
 
 const contacts = [
     {
-        icon: <PhoneIcon className="w-7 h-7 text-blue-500" />,
+        icon: <PhoneIcon className="text-blue-500 w-7 h-7" />,
         text: "+36 70 779 43 67",
         info: "H-K: 13:00-16:00, SZ-CS: 09:00-12:00",
     },
     {
-        icon: <AtSymbolIcon className="w-7 h-7 text-blue-500" />,
+        icon: <AtSymbolIcon className="text-blue-500 w-7 h-7" />,
         text: "csosziendoszkopia@gmail.com",
     },
     {
-        icon: <HomeIcon className="w-7 h-7 text-blue-500" />,
+        icon: <HomeIcon className="text-blue-500 w-7 h-7" />,
         text: "6000 Kecskemét, Faragó Béla fasor 4",
     },
 ];
@@ -25,13 +25,13 @@ const Contacts = () => {
                     Időpontot online tud foglalni, az{" "}
                     <a
                         className="mt-5 text-2xl font-bold"
-                        href="https://csosziendoszkopia.hu/online-bejelentkezes"
+                        href="/online-bejelentkezes"
                     >
                         "Online időpontfoglalás"
                     </a>{" "}
                     menüpont alatt. A rendelőben kötelező a maszk használata.
                 </p>
-                <p className="mb-5 text-xl text-white text-center">
+                <p className="mb-5 text-xl text-center text-white">
                     <strong>
                         Telefonos ügyfélszolgálatunk az alábbi időpontokban
                         elérhető.
@@ -42,18 +42,18 @@ const Contacts = () => {
             </div>
             <ul
                 role="list"
-                className="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-1 sm:gap-6 lg:grid-cols-3"
+                className="grid grid-cols-1 gap-5 mt-3 sm:grid-cols-1 sm:gap-6 lg:grid-cols-3"
             >
                 {contacts.map((contact) => (
                     <li
                         key={contact.text}
-                        className="col-span-1 flex shadow-sm"
+                        className="flex col-span-1 shadow-sm"
                     >
-                        <div className="flex w-16 flex-shrink-0 items-center justify-center text-sm font-medium text-white bg-white">
+                        <div className="flex items-center justify-center flex-shrink-0 w-16 text-sm font-medium text-white bg-white">
                             {contact.icon}
                         </div>
-                        <div className="flex flex-1 items-center justify-between truncate border-b border-r border-t border-gray-200 bg-white">
-                            <div className="flex-1 truncate px-4 py-2 text-md">
+                        <div className="flex items-center justify-between flex-1 truncate bg-white border-t border-b border-r border-gray-200">
+                            <div className="flex-1 px-4 py-2 truncate text-md">
                                 <a
                                     href="#"
                                     className="font-medium text-gray-900 hover:text-gray-600"
